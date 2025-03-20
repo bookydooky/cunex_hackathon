684c1668fe68031c267ea447e731f032a9cc93c5
bookydooky <bleechasan2546@gmail.com>
2025-03-20 23:12:32 +0700

    take out uneccessary imports

--- my-cunex-app/src/app/work/feedback/page.tsx
+++ my-cunex-app/src/app/work/feedback/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import React from 'react';
-import { ArrowLeft, Share2, MessageCircle } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 
 const WorkFeedback = () => {
