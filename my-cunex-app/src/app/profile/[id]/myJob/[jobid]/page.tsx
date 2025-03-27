22f0b82604d3c8d7509b218e749667ea25931240
pasutchien <pasut.chien@gmail.com>
2025-03-27 19:17:55 +0700

    message fix 1

--- my-cunex-app/src/app/profile/[id]/myJob/[jobid]/page.tsx
+++ my-cunex-app/src/app/profile/[id]/myJob/[jobid]/page.tsx
@@ -281,7 +281,10 @@ export default function MyJobsPage() {
 
                   {/* Action buttons */}
                   <div className="flex border-t border-gray-100">
-                    <button className="flex-1 py-3 text-center border-r border-gray-100 text-gray-500">
+                    <button
+                      className="flex-1 py-3 text-center border-r border-gray-100 text-gray-500"
+                      onClick={() => router.push("/chatpage")}
+                    >
                       Message
                     </button>
 
